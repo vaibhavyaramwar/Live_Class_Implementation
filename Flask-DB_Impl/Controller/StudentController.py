1eb9214c91730c649efb66b61181c1eb1a557fb3
Vaibhav Yaramwar <vaibhav.yaramwar@gmail.com>
2022-04-03 11:11:13 +0530

    MONGO DB and Flask Implementation

--- Flask-DB_Impl/Controller/StudentController.py
+++ Flask-DB_Impl/Controller/StudentController.py
@@ -1,15 +1,22 @@
 from DB import SqlDb
+from DB import MongoDB
 from flask import Flask, request, jsonify,json
 
 app = Flask(__name__)
 
 
 @app.route("/getStudentDetails", methods=["GET"])
-def getStudentDetails():
+def getStudentSQLDBDetails():
     studentlist = SqlDb.get_StudentData()
-    #dict(studentlist)
     return jsonify({"result":studentlist})
 
+@app.route("/getStudentData",methods=["GET"])
+def getStudentMongoDBDetails():
+    studentdata = MongoDB.getStudentData()
+    print(studentdata)
+    #return "success"
+    return jsonify({"result":str(studentdata)})
 
 if __name__ == '__main__':
     app.run()
+
