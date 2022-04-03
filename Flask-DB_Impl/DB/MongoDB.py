334b1364f3023a85d817e4ecd81314d116518cdb
Vaibhav Yaramwar <vaibhav.yaramwar@gmail.com>
2022-04-03 11:07:49 +0530

    MONGO DB and Flask Implementation

---
+++ Flask-DB_Impl/DB/MongoDB.py
@@ -0,0 +1,12 @@
+import pymongo
+
+
+def getStudentData():
+    client = pymongo.MongoClient("mongodb+srv://vyaramwar:xxxx@vaibhavycluster.llbsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db2=client['mongotest']
+    ineuron_collection = db2["ineuron_collection"]
+    result=[]
+    for i in ineuron_collection.find():
+        result.append(i)
+
+    return result
