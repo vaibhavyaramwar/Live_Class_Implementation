d0a0c2731706a9309914ede7517d8b6f36a8ebb0
Vaibhav Yaramwar <vaibhav.yaramwar@gmail.com>
2022-04-18 10:46:42 +0530

    Commit changes

--- Flask-DB_Impl/DB/MongoDB.py
+++ Flask-DB_Impl/DB/MongoDB.py
@@ -2,7 +2,7 @@ import pymongo
 
 
 def getStudentData():
-    client = pymongo.MongoClient("mongodb+srv://vyaramwar:xxxx@vaibhavycluster.llbsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://vyaramwar:Saptgiri12@vaibhavycluster.llbsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db2=client['mongotest']
     ineuron_collection = db2["ineuron_collection"]
     result=[]
